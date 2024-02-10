81a1f60ac4a9f793cab7cf07d864be46da20ee7f
Vlad_Milas <12milash12@gmail.com>
2024-02-10 21:15:03 +0300

    implement HW_1

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,22 +1,20 @@
-public class Game {
+public class MyFirstGame {
     public static void main(String[] args) {
-        int number = 25;
-        int userNumber = 100;
-        if (userNumber == number) {
-            System.out.println("Вы победили");
-        }
-        else if (userNumber > number) {
-            System.out.println("Число " + userNumber + " больше того, что загадал компьютер");
-            userNumber = 0;
-            while (userNumber != number) {
-                userNumber++;
-                if (userNumber < number) {
-                    System.out.println("Число " + userNumber + " меньше того, что загадал компьютер");
-                }
-                if (userNumber == number) {
-                    System.out.println("Вы победили");
-                }
+        int computerNumber = 90;
+        int playerNumber = 100;
+        if (playerNumber > computerNumber) {
+            while (playerNumber != computerNumber) {
+                System.out.println("Число " + playerNumber + " больше того, что загадал компьютер");
+                playerNumber--;
             }
+        } else if (playerNumber < computerNumber) {
+            while (playerNumber != computerNumber) {
+                System.out.println("Число " + playerNumber + " меньше того, что загадал компьютер");
+                playerNumber++;
+            }
+        }
+        if (playerNumber == computerNumber) {
+            System.out.println("Вы победили");
         }
     }
 }
