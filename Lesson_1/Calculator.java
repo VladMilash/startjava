991dcd8350fd955579f38a00591f456c5afcdb23
Vlad_Milas <12milash12@gmail.com>
2024-02-10 20:21:49 +0300

    implement HW_1

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,32 +1,24 @@
 public class Calculator {
     public static void main(String[] args) {
-        float number1 = 5f;
-        float number2 = 2f;
-        float a = number1;
-        float b = number2;
-        char plus = '+';
-        char minus = '-';
-        char multiply = '*';
-        char divide = '/';
-        char exponentiation = '^';
-        char divisionModulus = '%';
-        char sign = exponentiation;
+        float a = 10f;
+        float b = 2f;
+        char sign = '^';
         float result = 0f;
         if ((a > 0) && (b > 0)) {
-            if (sign == plus) {
+            if (sign == '+') {
                 result = a + b;
-            } else if (sign == minus) {
+            } else if (sign == '-') {
                 result = a - b;
-            } else if (sign == multiply) {
+            } else if (sign == '*') {
                 result = a * b;
-            } else if (sign == divide) {
+            } else if (sign == '/') {
                 result = a / b;
-            } else if (sign == divisionModulus) {
+            } else if (sign == '%') {
                 result = a % b;
-            } else if (sign == exponentiation) {
+            } else if (sign == '^') {
                 result = a;
                 for (int i = 1; i != b; i++) {
-                    result = result * a;
+                    result *= a;
                 } 
             } System.out.println(a + " " + sign + " " + b + " = " + result);
         }    
