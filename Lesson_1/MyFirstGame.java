8a8c9552cbb63a6d2c61deaf7c73ab3b950b83b2
Vlad_Milas <12milash12@gmail.com>
2024-02-14 12:44:20 +0300

    implement HW_1

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -3,11 +3,10 @@ public class MyFirstGame {
         int computerNumber = 5;
         int playerNumber = 1;
         while (computerNumber != playerNumber) {
-            if (computerNumber > playerNumber) {
+            if (playerNumber < computerNumber ) {
                 System.out.println("Число " + playerNumber + " меньше того, что загадал компьютер");
                 playerNumber++;
-            }
-            else if (computerNumber < playerNumber) {
+            } else  {
                 System.out.println("Число " + playerNumber + " больше того, что загадал компьютер");
                 playerNumber--;
             }
