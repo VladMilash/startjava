c0fe07c62985e932bbef64fbc0d4937a5e7c1ecc
Vlad_Milas <12milash12@gmail.com>
2024-02-21 19:35:17 +0300

    implement HW_1

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,10 +1,9 @@
 public class Calculator {
     public static void main(String[] args) {
-        float a = 10f;
-        float b = 2f;
+        float a = 2f;
+        float b = 10f;
         char sign = '^';
         float result = 0f;
-        if ((a > 0) && (b > 0)) {
             if (sign == '+') {
                 result = a + b;
             } else if (sign == '-') {
@@ -16,12 +15,11 @@ public class Calculator {
             } else if (sign == '%') {
                 result = a % b;
             } else if (sign == '^') {
-                result = a;
-                for (int i = 1; i != b; i++) {
+                result = 1;
+                for (int i = 0; i < b; i++) {
                     result *= a;
                 } 
             } 
             System.out.println(a + " " + sign + " " + b + " = " + result);
-        }    
     }
 }
