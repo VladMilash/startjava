0e45d4d4036c81cc30376d7ae541ca6295943e99
Vlad_Milas <12milash12@gmail.com>
2024-02-22 14:01:52 +0300

    implement HW_1

---
+++ Lesson_2/Person.java
@@ -0,0 +1,24 @@
+public class Person {
+    
+    String sex = "Мужской"
+    String name = "Иван";
+    int height = 183;
+    int weight = 70;
+    int age = 20;
+
+    public void go() {
+        System.out.println("Идти");
+    }
+
+    public void sit() {
+        System.out.println("Сидеть");
+    }
+
+    public void run() {
+        System.out.println("Бежать");
+    }
+
+    public void speak() {
+        System.out.println("Говорить");
+    }
+}
