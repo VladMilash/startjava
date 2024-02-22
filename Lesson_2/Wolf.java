021992b1354542579d4a9200dd0e2dff4fdd39d9
Vlad_Milas <12milash12@gmail.com>
2024-02-22 18:05:57 +0300

    implement HW_2

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,28 @@
+public class Wolf {
+    
+    String sex;
+    String nickname;
+    int weight;
+    int age;
+    String color;
+
+    public void go() {
+        System.out.println("Идет");
+    }
+
+    public void sit() {
+        System.out.println("Сидит");
+    }
+
+    public void run() {
+        System.out.println("Бежит");
+    }
+
+    public void howl() {
+        System.out.println("Воет");
+    }
+
+    public void hunting() {
+        System.out.println("Охотится");
+    }
+}
---
+++ Lesson_2/WolfTest.java
@@ -0,0 +1,23 @@
+public class WolfTest {
+
+    public static void main(String[] args) {
+        Wolf WolfOne = new Wolf();
+        WolfOne.sex = "Мужской";
+        WolfOne.nickname = "One";
+        WolfOne.weight = 2;
+        WolfOne.age = 4;
+        WolfOne.color = "black";
+
+        System.out.println("Sex = " + WolfOne.sex + 
+                "\nnickname = " + WolfOne.nickname +
+                "\nweight = " + WolfOne.weight +
+                "\nage = " + WolfOne.age +
+                "\ncolor = " + WolfOne.color + "\n");
+
+        WolfOne.go();
+        WolfOne.sit();
+        WolfOne.run();
+        WolfOne.howl();
+        WolfOne.hunting();
+    }
+}
