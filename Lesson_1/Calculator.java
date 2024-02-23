9be41946297a5fc60f3ce951696741d244cf8757
Vlad_Milas <12milash12@gmail.com>
2024-02-23 15:53:17 +0300

    implement HW_1

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -4,22 +4,22 @@ public class Calculator {
         float b = 10f;
         char sign = '^';
         float result = 0f;
-            if (sign == '+') {
-                result = a + b;
-            } else if (sign == '-') {
-                result = a - b;
-            } else if (sign == '*') {
-                result = a * b;
-            } else if (sign == '/') {
-                result = a / b;
-            } else if (sign == '%') {
-                result = a % b;
-            } else if (sign == '^') {
-                result = 1;
-                for (int i = 0; i < b; i++) {
-                    result *= a;
-                } 
+        if (sign == '+') {
+            result = a + b;
+        } else if (sign == '-') {
+            result = a - b;
+        } else if (sign == '*') {
+            result = a * b;
+        } else if (sign == '/') {
+            result = a / b;
+        } else if (sign == '%') {
+            result = a % b;
+        } else if (sign == '^') {
+            result = 1;
+            for (int i = 0; i < b; i++) {
+                result *= a;
             } 
-            System.out.println(a + " " + sign + " " + b + " = " + result);
+        } 
+        System.out.println(a + " " + sign + " " + b + " = " + result);
     }
 }
