3a607e4ddbfb82f32e37f4f22440676987041778
Vlad_Milas <12milash12@gmail.com>
2024-02-25 14:49:33 +0300

    implement HW_2

---
+++ Lesson_2/Jaeger.java
@@ -0,0 +1,89 @@
+public class Jaeger {
+
+    private String nickname;
+    private String mark;
+    private int height;
+    private int weight;
+    private int speed;
+    private int strength;
+    private int armor;
+    static int count = 0;
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public Jaeger() {
+        System.out.println("Создан новый робот");
+        count++;
+    }
+
+    public Jaeger(String nickname, String mark, int height, int weight, 
+                int speed, int strength, int armor) {
+        System.out.println("Создан новый робот");
+        this.nickname = nickname;
+        this.mark = mark;
+        this.height = height;
+        this.weight = weight;
+        this.speed = speed;
+        this.strength = strength;
+        this.armor = armor;
+        count++;
+    }
+
+    public void go() {
+        System.out.println("Идет");
+    }
+
+    public void run() {
+        System.out.println("Бежит");
+    }
+
+    public void guard() {
+        System.out.println("Охраняет");
+    }
+
+    public void attack() {
+        System.out.println("Атакует");
+    }
+
+    public void hide() {
+        System.out.println("Прячется");
+    }
+
+    public void countRobots() {
+        System.out.println("Роботов всего создано: " + count);
+    }
+}
---
+++ Lesson_2/JaegerTest.java
@@ -0,0 +1,42 @@
+public class JaegerTest {
+
+    public static void main(String[] args) {
+        Jaeger minotavr = new Jaeger();
+        String nickname = "Minotavr";
+        minotavr.setNickname(nickname);
+
+        String mark = "m-123";
+        minotavr.setMark(mark);
+
+        int height = 5;
+        minotavr.setHeight(height);
+
+        int weight = 40;
+        minotavr.setWeight(weight);
+
+        int speed = 10;
+        minotavr.setSpeed(speed);
+
+        int strenght = 50;
+        minotavr.setStrength(strenght);
+
+        int armor = 20;
+        minotavr.setArmor(armor);
+
+        System.out.println("Его имя: " + minotavr.getNickname() + 
+                    ", его марка: " + minotavr.getMark());
+        
+        minotavr.guard();
+        minotavr.go();
+
+        Jaeger scout = new Jaeger("Scout", "m-222", 4, 30, 25, 30, 10);
+        System.out.println("Его имя: " + scout.getNickname() + 
+                    ", его марка: " + scout.getMark());
+
+        scout.hide();
+        scout.run();
+        scout.attack();
+
+        scout.countRobots();
+    }
+}
