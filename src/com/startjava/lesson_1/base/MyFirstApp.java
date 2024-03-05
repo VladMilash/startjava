30fd75011af3bc4cb6221408e7672ece4bf4154d
Vlad_Milas <12milash12@gmail.com>
2024-03-05 13:29:54 +0300

    Test IdeaCommit

--- src/com/startjava/lesson_1/base/MyFirstApp.java
+++ src/com/startjava/lesson_1/base/MyFirstApp.java
@@ -1,7 +1,7 @@
 package com.startjava.lesson_1.base;
 
 public class MyFirstApp {
-	public static void main(String[] args) {
-		System.out.println("Привет, мир");
-	}
+    public static void main(String[] args) {
+        System.out.println("Привет, мир!!!");
+    }
 }
