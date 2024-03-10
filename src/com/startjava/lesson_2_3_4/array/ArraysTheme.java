f79cc82841aab7e9f2b528f921e2f664aa03f455
Vlad_Milas <12milash12@gmail.com>
2024-03-10 15:13:57 +0300

    Implement HW_4

--- src/com/startjava/lesson_2_3_4/array/ArraysTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArraysTheme.java
@@ -12,71 +12,78 @@ public class ArraysTheme {
         fillUniqueNumbersArray();
     }
 
-    public static void reverseArray() {
+    private static void reverseArray() {
         System.out.println("1. Реверс значений массива");
-        int[] reverseNum = {2, 1, 3, 5, 4, 6, 7};
+        int[] integers = {2, 1, 3, 5, 4, 6, 7};
         System.out.print("До реверса: ");
-        printArray(reverseNum, true, false, true);
+        printArray(integers);
+
+        int leftIndex = 0;
+        int rightIndex = integers.length - 1;
+        while (leftIndex < rightIndex) {
+            int temp = integers[leftIndex];
+            integers[leftIndex] = integers[rightIndex];
+            integers[rightIndex] = temp;
+            leftIndex++;
+            rightIndex--;
+        }
         System.out.print("\nПосле реверса: ");
-        printArray(reverseNum, true, true, true);
+        printArray(integers);
     }
 
-    public static void calculateFactorial() {
-        System.out.println("\n2. Вычисление факториала");
-        int[] calculatedNum = new int[10];
-        for (int i = 0; i < calculatedNum.length; i++) {
-            calculatedNum[i] = i;
+    private static void calculateFactorial() {
+        System.out.println("\n\n2. Вычисление факториала");
+        int[] integers = new int[10];
+        int length = integers.length;
+        for (int i = 0; i < length; i++) {
+            integers[i] = i;
         }
         int factorial = 1;
-        for (int i : calculatedNum) {
-            if ((i > 0) && (i <= 8)) {
-                factorial *= calculatedNum[i];
-            }
-            System.out.print((i > 0 && i < 8) ? (calculatedNum[i] + " * ")
-                    : (i == 8 ? calculatedNum[i] + " = " + factorial : ""));
+        for (int i = 1; i <= length-2; i++) {
+            factorial *= i;
+            System.out.print(integers[i]);
+            System.out.print(i < length - 2 ? " * " : " = " + factorial);
         }
     }
 
-    public static void removeArrayElements() {
+    private static void removeArrayElements() {
         System.out.println("\n\n3. Удаление элементов массива");
         Random random = new Random();
-        double[] originalNum = new double[15];
-        int lenght = originalNum.length;
+        double[] originalNums = new double[15];
+        int lenght = originalNums.length;
         for (int i = 0; i < lenght; i++) {
-            originalNum[i] = random.nextDouble();
+            originalNums[i] = random.nextDouble();
         }
         System.out.println("Исходный массив:");
-        printfArray(originalNum, "%.3f ", 8);
+        printfArray(originalNums, "%.3f ", 8);
 
-        double middleNum = originalNum[(lenght - 1) / 2];
+        double middleNum = originalNums[(lenght - 1) / 2];
         int counterZeroedCells = 0;
         for (int i = 0; i < lenght; i++) {
-            if (originalNum[i] > middleNum) {
-                originalNum[i] = 0;
+            if (originalNums[i] > middleNum) {
+                originalNums[i] = 0;
                 counterZeroedCells++;
             }
         }
         System.out.println("\nИзмененный массив:");
-        printfArray(originalNum, "%.3f ", 8);
+        printfArray(originalNums, "%.3f ", 8);
 
         System.out.println("\nКоличество обнуленных ячеек = " + counterZeroedCells);
     }
 
-    public static void printAlphabetStaircase() {
+    private static void printAlphabetStaircase() {
         System.out.println("\n4. Вывод алфавита лесенкой");
-        char[] letters = new char[26];
-        int lenght = letters.length;
-        char firstLatter = 65;
+        char[] alphabetLetters = new char[26];
+        int lenght = alphabetLetters.length;
         for (int i = 0; i < lenght; i++) {
-            letters[i] = firstLatter;
-            firstLatter++;
+            alphabetLetters[i] = (char) ('A' + i);
         }
         int counterLines = 1;
         int counterOutputLetters = 0;
         while (counterLines <= lenght) {
             while (counterOutputLetters != counterLines) {
                 for (int i = lenght - 1; counterOutputLetters != counterLines; i--) {
-                    System.out.print(letters[i]);
+                    System.out.print(alphabetLetters[i]);
                     counterOutputLetters++;
                 }
             }
@@ -86,64 +93,32 @@ public class ArraysTheme {
         }
     }
 
-    public static void fillUniqueNumbersArray() {
+    private static void fillUniqueNumbersArray() {
         System.out.println("\n5. Заполнение массива уникальными числами");
         Random random = new Random();
-        int[] uniqueNum = new int[30];
+        int[] uniqueNums = new int[30];
         int randomNum;
         boolean isUnique;
-        for (int i = 0; i < uniqueNum.length; i++) {
+        for (int i = 0; i < uniqueNums.length; i++) {
             do {
                 randomNum = random.nextInt(41) + 60;
                 isUnique = true;
                 for (int j = 0; j < i; j++) {
-                    if (uniqueNum[j] == randomNum) {
+                    if (uniqueNums[j] == randomNum) {
                         isUnique = false;
                         break;
                     }
                 }
             } while (!isUnique);
-            uniqueNum[i] = randomNum;
+            uniqueNums[i] = randomNum;
         }
-        Arrays.sort(uniqueNum);
-        printfArray(uniqueNum, " %s", 10);
+        Arrays.sort(uniqueNums);
+        printfArray(uniqueNums, " %s", 10);
     }
 
-    private static void printArray(int[] array, boolean isPrintBracket, boolean isReverseOutput, boolean isPrintComma) {
-        int lenght = array.length;
-        if (isPrintBracket) {
-            System.out.print("[");
-        }
-        if (!isReverseOutput) {
-            for (int i = 0; i < lenght; i++) {
-                System.out.print(array[i]);
-                if (isPrintComma) {
-                    if (i < lenght - 1) {
-                        System.out.print(", ");
-                    }
-                } else {
-                    if (i < lenght - 1) {
-                        System.out.print(" ");
-                    }
-                }
-            }
-        }
-        if (isReverseOutput) {
-            for (int i = lenght - 1; i >= 0; i--) {
-                System.out.print(array[i]);
-                if (isPrintComma) {
-                    if (i > 0) {
-                        System.out.print(", ");
-                    }
-                } else {
-                    if (i > 0) {
-                        System.out.print(" ");
-                    }
-                }
-            }
-        }
-        if (isPrintBracket) {
-            System.out.println("]");
+    private static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
         }
     }
 
