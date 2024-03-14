308d7ac73a4b82ee1de32510f46bd8aee425aec2
Vlad_Milas <12milash12@gmail.com>
2024-03-14 08:50:05 +0300

    Implement HW_4

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,24 +2,23 @@ package com.startjava.lesson_2_3_4.calculator;
 
 import java.text.DecimalFormat;
 
-
 public class Calculator {
-    private String mathExpression;
 
-    public void setMathExpression(String mathExpression) {
-        this.mathExpression = mathExpression;
-    }
-
-    public void calculete() {
-        String[] mathExpressionArray;
-        mathExpressionArray = mathExpression.split(" ");
-        if (mathExpressionArray.length != 3) {
+    public double calculete(String mathExpression) {
+        String[] elements = mathExpression.split(" ");
+        if (elements.length != 3) {
             System.out.println("Ошибка: между цифрами и математическим знаком должен быть пробел");
-            return;
+            return Double.NaN;
         }
-        int a = Integer.parseInt(mathExpressionArray[0]);
-        int b = Integer.parseInt(mathExpressionArray[mathExpressionArray.length - 1]);
-        char sign = mathExpressionArray[1].charAt(0);
+        int a = Integer.parseInt(elements[0]);
+        int b = Integer.parseInt(elements[elements.length - 1]);
+        char sign = elements[1].charAt(0);
+
+        if ((sign != '+') && (sign != '-') && (sign != '*') && (sign != '/') && (sign != '%') && (sign != '^')) {
+            System.out.println("Ошибка: неверный математический знак");
+            return Double.NaN;
+        }
+
         double result;
 
         switch (sign) {
@@ -42,8 +41,7 @@ public class Calculator {
                 result = (double) Math.pow(a, b);
                 break;
             default:
-                System.out.println("Ошибка: знак " + sign + " не поддерживается");
-                return;
+                return Double.NaN;
         }
 
         System.out.print(a + " " + sign + " " + b + " = ");
@@ -54,5 +52,6 @@ public class Calculator {
             DecimalFormat format2 = new DecimalFormat("#.###");
             System.out.println(format2.format(result));
         }
+        return result;
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -7,31 +7,17 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Calculator calculatorOne = new Calculator();
         Scanner scanner = new Scanner(System.in);
-        String choice;
+        String choice = "yes";
+
         do {
-            System.out.print("Введите математическое выражение: ");
-            String mathExpression = scanner.nextLine();
-            calculatorOne.setMathExpression(mathExpression);
-
-            calculatorOne.calculete();
-            choice = getUserChoice();
-
-        } while (!choice.equals("no"));
-        System.out.println("Программа завершена. Всего доброго!");
-    }
-
-    private static String getUserChoice() {
-        String choice;
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
+            if (choice.equals("yes")) {
+                System.out.print("Введите математическое выражение: ");
+                String mathExpression = scanner.nextLine();
+                calculatorOne.calculete(mathExpression);
+            }
             System.out.println("Хотите продолжить вычисления? [yes/no]:");
             choice = scanner.nextLine();
-            if (choice.equals("yes") || choice.equals("no")) {
-                break;
-            } else {
-                System.out.println("Ведите нужное слово: [yes/no]:");
-            }
-        }
-        return choice;
+        } while (!choice.equals("no"));
+        System.out.println("Программа завершена. Всего доброго!");
     }
 }
